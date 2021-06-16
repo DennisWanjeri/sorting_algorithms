82902fbf83d630165387c40374f3ef4ededcaed0
root <DennisWanjeri>
2021-06-16 17:20:49 +0000

    104

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -53,7 +53,7 @@ void heap_sort(int *array, size_t size)
 	for (i = size / 2 - 1; i >= 0; i--)
 		heapify(array, size, i, tmp);
 	/*heap sort*/
-	for (i = size - 1; i >= 0; i--)
+	for (i = size - 1; i > 0; i--)
 	{
 		swap(&array[0], &array[i]);
 		print_array(array, tmp);
